8e2db7f081bd72c171b7cad9c0585066b990763b
ashrafbaker <ashraf.baker.extern@dvag.com>
2023-06-20 09:50:42 +0200

    first commit

---
+++ src/test/kotlin/org/abulasa/actions/ActionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.abulasa.actions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
